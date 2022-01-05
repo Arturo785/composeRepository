77e1ee080483b7956f741b32f36bc57a1a0255a3
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2022-01-05 14:34:12 -0600

    addition of basic stuff and starter proyect

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -1,24 +1,58 @@
 package com.example.composelearning
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // columns run vertically
+        // rows run horizontally
         setContent {
-            ComposeLearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    
-                }
+            Column(
+                modifier = Modifier
+                    // the changes are called sequentially
+                    .background(Color.Cyan)
+                    .fillMaxHeight(.5f)
+                    .width(300.dp) // this fills but does not overflow
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Green)
+                    .padding(5.dp)
+                //    .requiredWidth(600.dp) this fills and overflows
+            ) {
+                Text(
+                    text = "Hello",
+                    modifier = Modifier.clickable {
+                        Toast.makeText(
+                            this@MainActivity, "clicked",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                ) // gives space but does not push items pos is x,y
+                Spacer(modifier = Modifier.height(50.dp)) // sets a space
+                Text(text = "world")
             }
         }
     }
