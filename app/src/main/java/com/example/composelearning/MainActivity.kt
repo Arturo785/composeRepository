c897e883a2286e71f64f047ddd22758970bfce9f
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2022-01-05 23:12:18 -0600

    basic creation and handle of the state

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -12,12 +12,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelearning.ui.theme.ComposeLearningTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,48 +29,62 @@ class MainActivity : ComponentActivity() {
         // columns run vertically
         // rows run horizontally
         setContent {
-            Column(
-                modifier = Modifier
-                    // the changes are called sequentially
-                    .background(Color.Cyan)
-                    .fillMaxHeight(.5f)
-                    .width(300.dp) // this fills but does not overflow
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Yellow)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Green)
-                    .padding(5.dp)
-                //    .requiredWidth(600.dp) this fills and overflows
-            ) {
-                Text(
-                    text = "Hello",
-                    modifier = Modifier.clickable {
-                        Toast.makeText(
-                            this@MainActivity, "clicked",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                ) // gives space but does not push items pos is x,y
-                Spacer(modifier = Modifier.height(50.dp)) // sets a space
-                Text(text = "world")
+            Column(Modifier.fillMaxSize()) {
+
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                    // every time we touch the box the fun gets called and in here we
+                    // receive what was sent as param
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
             }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeLearningTheme {
-        Greeting("Android")
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (color: Color) -> Unit) {
+
+    //thanks to this it will hold the value no matter how many times it is called
+/*
+    val color = remember {
+        mutableStateOf(Color.Yellow)
     }
+*/
+
+    Box(
+        modifier = modifier
+            .background(
+                Color.Red
+            )
+            .clickable {
+                // calls the fun that we passed to be executed and assigns the param value
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                    )
+                )
+            }
+    )
+
 }
+
