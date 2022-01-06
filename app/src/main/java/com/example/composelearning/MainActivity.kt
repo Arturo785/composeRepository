40dfac7a77ca4b0b4faf3c9e051d7cd2598278c0
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2022-01-06 17:39:17 -0600

    button, editText and snackBar done

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.example.composelearning
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composelearning.ui.theme.ComposeLearningTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,48 +19,58 @@ class MainActivity : ComponentActivity() {
         // columns run vertically
         // rows run horizontally
         setContent {
-            Column(
-                modifier = Modifier
-                    // the changes are called sequentially
-                    .background(Color.Cyan)
-                    .fillMaxHeight(.5f)
-                    .width(300.dp) // this fills but does not overflow
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Yellow)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Green)
-                    .padding(5.dp)
-                //    .requiredWidth(600.dp) this fills and overflows
-            ) {
-                Text(
-                    text = "Hello",
-                    modifier = Modifier.clickable {
-                        Toast.makeText(
-                            this@MainActivity, "clicked",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                ) // gives space but does not push items pos is x,y
-                Spacer(modifier = Modifier.height(50.dp)) // sets a space
-                Text(text = "world")
+
+            val scaffoldState = rememberScaffoldState()
+
+            val scope = rememberCoroutineScope()
+
+            // to handle the state of our input text
+            var textFieldState by remember {
+                mutableStateOf("")
             }
+
+            // provides the layout that gets along with material design things
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text("Enter your name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        // never launch coroutine directly in a composable
+                        // is ok in callbacks such as in here
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar(
+                                "Hello $textFieldState"
+                            )
+                        }
+
+                    }) {
+                        Text(text = "Pls greet me")
+                    }
+                }
+
+            }
+
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeLearningTheme {
-        Greeting("Android")
     }
 }
